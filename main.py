4880e09bb197763e2720aa4261505f9c8346e570
stephen.easley-walsh <stephen.easley-walsh@studygroup.com>
2025-08-11 17:02:41 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# Question 1
+print("Question 1")
+print((12.4*5.9)/(3.7*2.5))
+print((42.3+18.4)/(16.7*0.4))
+print((81.73-21.4)/(3.87*1.25))
+
+# Question 2
+print("Question 2")
+print(30.5**0.5)
+print(348**0.5)
+print(107.8**0.5)
+print(3.9**2)
+print(16.34**2)
+
+# Question 3
+print("Question 3")
+print(32/(14**2))
+print((26**0.5)/4)
+print((2*40**0.5)/3)
+print((12.8**2)/3.7)
+print(15.4/(38**0.5))
+
+#Question 4
+print("Question 4")
+print(3.4**2+3/14.8+108**0.5)
+print((27**0.5)*(4/3.8)+9.62**2)
+print(73**0.5+((45**0.5)/3)*(6.8/14.9))
+print((6/15.9)*(136**0.5)-(5/18.7))
